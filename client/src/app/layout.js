31ebbce292db67f65a212b84fb3bdaf452a6feb5
ekaygun <103898376+ekaygun@users.noreply.github.com>
2024-12-29 21:30:08 -0500

    Create layout.js

---
+++ client/src/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Rezy',
+  description: 'Reselling Made Easy',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-emerald-50">{children}</body>
+    </html>
+  );
+}
