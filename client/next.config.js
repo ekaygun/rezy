e78b80ad3093762cc57269bddbb4e803b13b2b94
ekaygun <103898376+ekaygun@users.noreply.github.com>
2024-12-29 22:13:23 -0500

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://rezy-production.up.railway.app'
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true
   }
 }
 
