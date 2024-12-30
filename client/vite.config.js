d0fcabc3a55e9be4a3bf4e1a3aeef6504d9b9a67
ekaygun <103898376+ekaygun@users.noreply.github.com>
2024-12-30 01:32:17 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    strictPort: true
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT || 3000,
+    strictPort: true
   }
 })
