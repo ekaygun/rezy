0ee1c189506068b2054f0df0d4fb5c78e127374b
ekaygun <103898376+ekaygun@users.noreply.github.com>
2024-12-30 00:46:35 -0500

    Update layout.js

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,9 +1,14 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'Rezy',
+  description: 'Reselling Made Easy',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen bg-emerald-50">{children}</body>
     </html>
   );
 }
